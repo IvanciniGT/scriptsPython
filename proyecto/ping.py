3887389abb0b0a1a4279386d0090471d29839f9c
Ubuntu <ubuntu@ip-172-31-3-2.eu-west-1.compute.internal>
2021-11-10 14:02:21 +0000

    subprocesos

--- proyecto/ping.py
+++ proyecto/ping.py
@@ -1,5 +1,12 @@
+from subprocess import run, PIPE
+import platform     # Obtener información del SO donde estamos corriendo el programa
+
+
 def ping(servidor):
-    # True o false, dependiendo de si hace o no ping
-    
-print(   ping("google.es")   )
-print(   ping("googlecito.es")   )
+    parametro="-n" if platform.system().upper() == "WINDOWS" else "-c"
+    comando=( "ping" , parametro , "1", servidor )
+    resultado_de_su_ejecucion=run( comando , stdout=PIPE , stderr=PIPE)
+    return resultado_de_su_ejecucion.returncode==0
+
+#print(   ping("google.es")   )
+#print(   ping("googlecito.es")   )
